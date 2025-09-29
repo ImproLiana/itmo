2cf4de451a5708bba3709631213a6d49aaf8f76b
Uliana <97896659+ImproLiana@users.noreply.github.com>
2025-09-29 12:46:55 +0300

    Delete sem3/web/lab1/web/httpd-root/link.js

--- sem3/web/lab1/web/httpd-root/link.js
+++
@@ -1,6 +0,0 @@
-let link = `https://se.ifmo.ru/~s466221/`;
-let proxy = `proxy.php`;
-
-function getLink(x, y, r){
-    return link + proxy + `?x=${x.value}&y=${y.value.replace(/,/g, ".")}&r=${r.value}`;
-}
